1ba7d53b5c74e89328a22b0c9baaf4c5e3c2eb7a
chary <920868587@qq.com>
2024-09-21 17:56:06 +0800

    检查uniformid干嘛啊

--- api/log.ts
+++ api/log.ts
@@ -4,53 +4,53 @@ export const runtime = 'edge';
 // const { url } = await put('articles/blob.txt', 'Hello World!', { access: 'public' });
 const fronturl = 'https://story-painter-theta.vercel.app/'
 const filesizelimit = 2
-export async function GET(req:Request) {
-    const { key, password } = querystring.parse(req.url.replace(/^.+?\?/, ''))
-    return Response.redirect('https://uxle9woampkgealk.public.blob.vercel-storage.com/'+key+'-'+password)
+export async function GET(req: Request) {
+	const { key, password } = querystring.parse(req.url.replace(/^.+?\?/, ''))
+	return Response.redirect('https://uxle9woampkgealk.public.blob.vercel-storage.com/' + key + '-' + password)
 }
 export async function PUT(req: Request) {
 
-    const obj = await req.formData()
-    const file= obj.get('file') as File
-    if (!file) {
-        return Response.json({
-            data: 'no file error'
-        },{status: 400})
-    }
-    const { name,uniform_id,client } = querystring.parse(req.url.replace(/^.+?\?/, ''))
-
-
-		//检验uniform_id的正确性
-		var patt1 = /^[^:]+:\d+$/;
-		if (!patt1.test(uniform_id as string)) {
-			//返回未能通过的信息：uniform_id field did not pass validation
-			return Response.json({
-                data: "uniform_id field did not pass validation",
-            },{status: 400})
-		}
-
-		//检验file文件的大小
-		if (file.size > filesizelimit * 1024 * 1024) {
-			return Response.json({
-                data: "Size is too big!",
-            },{status: 400})
-		}
-
-
-		//转base64
-		let logdata = "";
-
-        logdata = new TextDecoder().decode(new Uint8Array(await file.arrayBuffer()))
-		logdata = btoa(logdata);
-		let key = generateRandomString(4);
-
-
-        const { url:fileUrl } = await put(`${key}`, JSON.stringify(generateStorageData(logdata, name as string)), { access: 'public' });
-		// 返回log地址
+	const obj = await req.formData()
+	const file = obj.get('file') as File
+	if (!file) {
 		return Response.json({
-            url: fronturl + '?key=' +key+'#'+fileUrl.replace(`https://uxle9woampkgealk.public.blob.vercel-storage.com/${key}-`,''),
-        })
-    // }
+			data: 'no file error'
+		}, { status: 400 })
+	}
+	const { name, uniform_id, client } = querystring.parse(req.url.replace(/^.+?\?/, ''))
+
+
+	//检验uniform_id的正确性
+	// var patt1 = /^[^:]+:\d+$/;
+	// if (!patt1.test(uniform_id as string)) {
+	// 	//返回未能通过的信息：uniform_id field did not pass validation
+	// 	return Response.json({
+	// 		data: "uniform_id field did not pass validation",
+	// 	}, { status: 400 })
+	// }
+
+	//检验file文件的大小
+	if (file.size > filesizelimit * 1024 * 1024) {
+		return Response.json({
+			data: "Size is too big!",
+		}, { status: 400 })
+	}
+
+
+	//转base64
+	let logdata = "";
+
+	logdata = new TextDecoder().decode(new Uint8Array(await file.arrayBuffer()))
+	logdata = btoa(logdata);
+	let key = generateRandomString(4);
+
+
+	const { url: fileUrl } = await put(`${key}`, JSON.stringify(generateStorageData(logdata, name as string)), { access: 'public' });
+	// 返回log地址
+	return Response.json({
+		url: fronturl + '?key=' + key + '#' + fileUrl.replace(`https://uxle9woampkgealk.public.blob.vercel-storage.com/${key}-`, ''),
+	})
+	// }
 }
 
 function generateRandomString(length: number) {
